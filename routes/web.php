293875f28891a0317db900e158b5701e3d301e75
AamirpalSehmi <aamirpal@ucreate.co.in>
2019-01-17 18:30:00 +0530

    auth added

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/test', function () {
 	\Log::debug('Here is some debug information');
     return view('jhgjg');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
